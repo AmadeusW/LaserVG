50f6c264adbafa0246f13092260c5306a8b54142
Amadeusz Wieczorek <git@amadeusw.com>
2018-04-04 22:57:32 -0700

    creates console app

---
+++ prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

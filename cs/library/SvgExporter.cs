0f105b28a2e484e0fba2ca1793269e7549af000b
Amadeusz Wieczorek <git@amadeusw.com>
2018-04-10 18:14:36 -0700

    svg export

--- cs/library/SvgExporter.cs
+++ cs/library/SvgExporter.cs
@@ -8,17 +8,39 @@ namespace Deo.LaserVg
 {
     class SvgExporter
     {
+        private const string svgUrl = "http://www.w3.org/2000/svg";
+        internal static readonly XNamespace ns = svgUrl;
+
         internal static void Export(IEnumerable<IPart> parts, string outputPath)
         {
+            Console.WriteLine("Exporting...");
+
             var document = new XDocument();
-            var root = new XElement("svg");
+            var root = new XElement("svg"//,
+                //new XAttribute("xmlns", svgUrl), // this doesn't work
+                // new XAttribute("width", "5cm")
+                );
+            document.Add(root);
+
             foreach (var part in parts)
             {
                 root.Add(part.ToXml());
             }
-            document.Add(root);
-            var writer = XmlWriter.Create(outputPath);
-            document.WriteTo(writer);
+
+            Console.WriteLine("Saving...");
+
+            var settings = new XmlWriterSettings()
+            {
+                Indent = true,
+                IndentChars = "  ",
+                NewLineChars = "\n"
+            };
+            using (var writer = XmlWriter.Create(outputPath, settings))
+            {
+                document.WriteTo(writer);
+            }
+
+            Console.WriteLine("Done.");
         }
     }
 }

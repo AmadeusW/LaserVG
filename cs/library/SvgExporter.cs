f81360a4ebfcf5b9724d9a5bc51aff0e3d319c90
Amadeusz Wieczorek <git@amadeusw.com>
2018-11-28 23:58:16 -0800

    embeds namespace information so that output SVG renders (FML)

--- cs/library/SvgExporter.cs
+++ cs/library/SvgExporter.cs
@@ -8,17 +8,13 @@ namespace Deo.LaserVg
 {
     class SvgExporter
     {
-        private const string svgUrl = "http://www.w3.org/2000/svg";
-        internal static readonly XNamespace ns = svgUrl;
-
         internal static void Export(Sketch sketch, string outputPath)
         {
             Console.WriteLine("Exporting...");
 
+            XNamespace ns = "http://www.w3.org/2000/svg";
             var document = new XDocument();
-            var root = new XElement("svg"//,
-                //new XAttribute("xmlns", svgUrl), // this doesn't work
-                );
+            var root = new XElement("svg");
             if (sketch.Width != 0)
                 root.SetAttributeValue("width", sketch.Width + sketch.Unit);
             if (sketch.Height != 0)
@@ -32,6 +28,7 @@ namespace Deo.LaserVg
             {
                 root.Add(part.ToXml());
             }
+            UnfuckXml(root, ns);
 
             Console.WriteLine("Saving...");
 
@@ -50,6 +47,25 @@ namespace Deo.LaserVg
             }
 
             Console.WriteLine("Done.");
+        }
+
+        /// <summary>
+        /// Recursively appends the <see cref="XNamespace"/> to every <see cref="XElement"/> of the tree.
+        /// SVG requires us to set the namespace on the root <svg> element.
+        /// The evil .NET XML implementation requires us to set the namespace on every element.
+        /// Without explicitly setting the namespace, svg elements would receive an empty namespace
+        /// and would not render. Of course, there is no way to set the default namespace for the entire document.
+        /// To work around this horrible mess, we traverse the entire tree and set the namespace.
+        /// </summary>
+        /// <param name="element">Visited <see cref="XElement"/></param>
+        /// <param name="ns">the <see cref="XNamespace"/> that needs to be added to every <see cref="XElement"/></param>
+        private static void UnfuckXml(XElement element, XNamespace ns)
+        {
+            element.Name = ns + element.Name.LocalName;
+            foreach (XElement child in element.Elements())
+            {
+                UnfuckXml(child, ns);
+            }
         }
 
         private static void EnsureDirectoryExists(string outputPath)

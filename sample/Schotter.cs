0873bde4f2cdb775bed94156e6e3ddaaa8f07613
Amadeusz Wieczorek <git@amadeusw.com>
2018-12-07 00:00:39 -0800

    improves schotter sample

--- sample/Schotter.cs
+++ sample/Schotter.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Deo.LaserVg;
+using System;
 
 namespace Deo.LaserVg.Sample
 {
     class Schotter
     {
-        double RotationFactor = 4;
-        double TranslationFactor = 0.2;
+        double RotationFactor = 0.6;
+        double TranslationFactor = 0.03;
         decimal Edge = 1.0m;
         int Width = 4;
         int Height = 7;
@@ -17,10 +16,10 @@ namespace Deo.LaserVg.Sample
 
         internal void Make(string outputPath)
         {
-            sketch = new Sketch() { Etching = true, Width = Width+Edge, Height = Height+Edge, Unit = "in" };
+            sketch = new Sketch() { Etching = true, Width = Width+2*Edge, Height = Height+2*Edge, Unit = "in" };
             random = new Random();
 
-            sketch.StartGroup("Schotter", $"translate({Edge/2}, {Edge / 2})");
+            sketch.StartGroup("Schotter", $"translate({Edge}, {Edge})"); // this group adds margin
             for (int row = 0; row < Height; row++)
                 for (int col = 0; col < Width; col++)
                     MakeSquare(row, col);
@@ -29,11 +28,13 @@ namespace Deo.LaserVg.Sample
             sketch.Save(outputPath);
         }
 
+        private double RandomInExponentialRange(double range) => random.NextDouble() * range * range - (range * range / 2);
+
         private void MakeSquare(int row, int col)
         {
-            var rotation = (random.NextDouble() * row - row * 0.5) * RotationFactor;
-            var dx = (decimal)((random.NextDouble() * row - row * 0.5) * TranslationFactor);
-            var dy = (decimal)((random.NextDouble() * row - row * 0.5) * TranslationFactor);
+            var rotation = RandomInExponentialRange(row) * RotationFactor;
+            var dx = (decimal)(RandomInExponentialRange(row) * TranslationFactor);
+            var dy = (decimal)(RandomInExponentialRange(row) * TranslationFactor);
 
             sketch.StartGroup(string.Empty, $"rotate({rotation} {col * Edge + Edge / 2} {row * Edge + Edge / 2}) translate({col * Edge + dx} {row * Edge + dy})");
             sketch.MoveTo(0, 0);
